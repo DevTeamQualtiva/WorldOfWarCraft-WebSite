975545b7794f051c4b67a625420fcb074dbc3440
One <projectonecore@gmail.com>
2013-08-13 09:09:25 -0600

    base de datos v2 actualizada incorporando los update

--- Sql/Update/11_08_13_wow_db_version.sql
+++
@@ -1,1 +0,0 @@
-UPDATE `wow_db_version` SET `version`='2' WHERE `version`='1';
--- Sql/Update/12_08_13_wow_main_menu.sql
+++
@@ -1,3 +0,0 @@
--- Actualización del nombre del menu
-UPDATE `wow_main_menu` SET `title_en`='Game Guide' WHERE `id`='2';
-
