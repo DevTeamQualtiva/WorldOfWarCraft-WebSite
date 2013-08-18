89af4c3168c7080af887cbb34ee8987fb5aa7b16
One <projectonecore@gmail.com>
2013-08-18 14:09:25 -0600

    Correcciones a la promo del sidebar

--- aplicacion/vista/wow/wow_block_bn_ads.php
+++ aplicacion/vista/wow/wow_block_bn_ads.php
@@ -1,26 +1,23 @@
 <!-- START: Battle.Net ADs -->
 <div>
-<div id="sidebar-marketing" class="sidebar-module">
-	<div class="bnet-offer">
-	<!-- try -->
-		<div class="bnet-offer-bg">
-				<a href="https://eu.battle.net/account/management/" target="_blank" id="ad10068081" class="bnet-offer-image" onclick="BnetAds.trackEvent('campaignId:10068081 - imgId:9914162', 'D3_Starter-EU', 'wow', true);">
-				<img src="//bnetcmsus-a.akamaihd.net/cms/ad_300x250/QEZ6E2FJR8XI1345548922316.jpg" width="300" height="250" alt="" />
-			</a>
+	<div id="sidebar-marketing" class="sidebar-module">
+		<div class="bnet-offer">
+		<!-- try -->
+			<div class="bnet-offer-bg">
+				<a href="<?php echo WoW::GetWoWPath(); ?>/account/landing.xml" class="free-trial png-fix" data-ad="Classic: Trial">
+					<img src="<?php echo WoW::GetWoWPath(); ?>/wow/static/images/sidebar/promo/<?php echo WoW_Locale::GetLocale(LOCALE_DOUBLE); ?>.jpg" alt="Promos" />
+				</a>
+			</div>
+
+			<script type="text/javascript">
+				//<![CDATA[
+					if(typeof (BnetAds.addEvent) != "undefined" )
+						BnetAds.addEvent(window, 'load', function(){ BnetAds.trackEvent('campaignId:10068081 - imgId:9914162', 'D3_Starter-EU', 'wow'); } );
+					else
+						BnetAds.trackEvent('10068081', 'D3_Starter-EU', 'wow');
+				//]]>
+			</script>
 		</div>
-
-
-		<script type="text/javascript">
-			//<![CDATA[
-				if(typeof (BnetAds.addEvent) != "undefined" )
-					BnetAds.addEvent(window, 'load', function(){ BnetAds.trackEvent('campaignId:10068081 - imgId:9914162', 'D3_Starter-EU', 'wow'); } );
-				else
-					BnetAds.trackEvent('10068081', 'D3_Starter-EU', 'wow');
-			//]]>
-		</script>
-</div>
-</div>
+	</div>
 </div>
 <!-- END: Battle.Net ADs -->
-
-
