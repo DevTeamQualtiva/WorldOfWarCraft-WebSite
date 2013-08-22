c80e394881029c973bdcc26c5c5123e765221493
One <projectonecore@gmail.com>
2013-08-22 16:48:15 -0600

    fixed footer -> /wow

--- aplicacion/vista/overall/overall_block_footer.php
+++ aplicacion/vista/overall/overall_block_footer.php
@@ -1,6 +1,6 @@
 <!-- START: Footer -->
 <div id="footer">
-<div id="sitemap"<?php echo WoW_Template::GetTemplateTheme() == 'wow' ? ' class="footer-ads"' : null; ?>>
+<div id="sitemap">
 <div class="column">
 <h3 class="bnet">
 <a href="http://eu.battle.net/" tabindex="100"><?php echo WoW_Locale::GetString('template_footer_home_title'); ?></a>
