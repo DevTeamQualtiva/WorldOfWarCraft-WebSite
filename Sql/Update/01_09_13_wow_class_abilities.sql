661d0dcc49f2b05914f2a1234a0421f467b54f48
One <projectonecore@gmail.com>
2013-09-01 16:07:16 -0600

    Se ve mas bonito asi :-)
    BOM!!!!

--- Sql/Update/01_09_13_wow_class_abilities.sql
+++ Sql/Update/01_09_13_wow_class_abilities.sql
@@ -9,7 +9,8 @@ UPDATE `wow_class_abilities` SET `id`='44' WHERE `id`='40';
 -- -------------------------------------------------------
 -- habilidades Para la Clase "Monk" Necesita su escritura
 -- -------------------------------------------------------
-INSERT INTO `wow_class_abilities`(`id`,`class`,`title_de`,`title_en`,`title_es`,`title_fr`,`title_ru`,`text_de`,`text_en`,`text_es`,`text_fr`,`text_ru`,`icon`) VALUES ( '37','10','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK');
-INSERT INTO `wow_class_abilities`(`id`,`class`,`title_de`,`title_en`,`title_es`,`title_fr`,`title_ru`,`text_de`,`text_en`,`text_es`,`text_fr`,`text_ru`,`icon`) VALUES ( '38','10','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK');
-INSERT INTO `wow_class_abilities`(`id`,`class`,`title_de`,`title_en`,`title_es`,`title_fr`,`title_ru`,`text_de`,`text_en`,`text_es`,`text_fr`,`text_ru`,`icon`) VALUES ( '39','10','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK');
-INSERT INTO `wow_class_abilities`(`id`,`class`,`title_de`,`title_en`,`title_es`,`title_fr`,`title_ru`,`text_de`,`text_en`,`text_es`,`text_fr`,`text_ru`,`icon`) VALUES ( '40','10','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK');
+INSERT INTO `wow_class_abilities`(`id`,`class`,`title_de`,`title_en`,`title_es`,`title_fr`,`title_ru`,`text_de`,`text_en`,`text_es`,`text_fr`,`text_ru`,`icon`) VALUES
+( '37','10','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK'),
+( '38','10','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK'),
+( '39','10','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK'),
+( '40','10','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK','MONK');
