3af18b98e6f3815b7e67c0ecdef74ff575c11ec9
One <projectonecore@gmail.com>
2013-09-01 16:38:43 -0600

    Actualizando el local es-es del menu principal

---
+++ Sql/Update/01_09_13_wow_main_menu.sql
@@ -0,0 +1,4 @@
+-- -----------------------------------------------
+-- Actualizando el local es-es del menu principal
+-- -----------------------------------------------
+UPDATE `wow_main_menu` SET `title_es`='Principal' WHERE `id`='1';
