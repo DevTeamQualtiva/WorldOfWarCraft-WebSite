d16a952fe758d5d31488e4759539f5c74e9fd440
One <projectonecore@gmail.com>
2013-09-01 16:12:02 -0600

    Habilidades para la raza "Pandaren" necesita su escritura a todas las habilidades de la raza pandaren a su idioma
    esto se hara poco a poco ya que no es prioridad pero si se reservaron
    los cambios respectivos en la bd

---
+++ Sql/Update/01_09_13_wow_race_abilities.sql
@@ -0,0 +1,8 @@
+-- -------------------------------------------------------
+-- habilidades Para la Raza "Pandaren" Necesita su escritura
+-- -------------------------------------------------------
+INSERT INTO `wow_race_abilities`(`id`,`race`,`title_de`,`title_en`,`title_es`,`title_fr`,`title_ru`,`text_de`,`text_en`,`text_es`,`text_fr`,`text_ru`,`icon`) VALUES 
+( '48','24','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren'),
+( '49','24','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren'),
+( '50','24','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren'),
+( '51','24','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren','Pandaren');
