8a3da9310cf148b69dc27a5e4879587f52d7e4c0
One <projectonecore@gmail.com>
2013-09-24 15:11:21 -0600

    Fixed error "GetLocale" link "what-is" page

--- aplicacion/vista/bn/bn_block_my_games_0.php
+++ aplicacion/vista/bn/bn_block_my_games_0.php
@@ -5,7 +5,7 @@
 			<span class="button-left"><span class="button-right"><?php echo WoW_Locale::GetString('template_bn_new_account'); ?></span></span></a><br />
 			<?php echo WoW_Locale::GetString('template_bn_got_account'); ?> <a href="?login" onclick="return Login.open('<?php echo WoW::GetWoWPath(); ?>/login/login.frag')"><?php echo WoW_Locale::GetString('template_bn_log_in'); ?></a>
 		</div>
-		<h2><a href="what-is/" id="learn-more"><?php echo WoW_Locale::GetString('template_bn_what_is_caption'); ?></a></h2>
+		<h2><a href="<?php echo WoW::GetWoWPath(); ?><?php echo WoW_Locale::GetLocale(); ?>/what-is/" id="learn-more" tabindex="1"><?php echo WoW_Locale::GetString('template_bn_what_is_caption'); ?></a></h2>
 	</div>
 <span class="clear"><!-- --></span>
 </div>
