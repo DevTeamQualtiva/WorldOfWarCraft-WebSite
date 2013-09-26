2c20cb52f2e028913d9839cc215627ca659c1a4d
One <projectonecore@gmail.com>
2013-09-26 17:22:43 -0600

    Add page docs
    añadiendo pagina de documentacion esta misma se seguira editando y
    añadiendo mas datos segun avance el CMS

---
+++ documentacion/static/js/BattleCMS.js
@@ -0,0 +1,33 @@
+/**
+ * BattleCMS.js
+ *
+ */
+//<![CDATA[
+var Core = Core || {},
+Login = Login || {};
+Core.staticUrl = 'http://eu.battle.net/static';
+Core.sharedStaticUrl = 'http://eu.battle.net/static/local-common';
+Core.baseUrl = 'http://eu.battle.net/es';
+Core.projectUrl = '';
+Core.cdnUrl = 'http://media.blizzard.com/';
+Core.supportUrl = 'http://eu.battle.net/support/';
+Core.secureSupportUrl = 'https://eu.battle.net/support/';
+Core.project = 'root';
+Core.locale = 'es-es';
+Core.language = 'es';
+Core.region = 'eu';
+Core.shortDateFormat = 'dd/MM/yyyy';
+Core.dateTimeFormat = 'dd/MM/yyyy HH:mm';
+Core.loggedIn = false;
+Login.embeddedUrl = 'https://eu.battle.net/login/login.frag';
+var Flash = Flash || {};
+Flash.videoPlayer = 'http://media.blizzard.com/global-video-player/themes/root/video-player.swf';
+Flash.videoBase = 'http://media.blizzard.com/root/media/videos';
+Flash.ratingImage = '../../../media.blizzard.com/global-video-player/ratings/root/es-es.html';
+Flash.expressInstall = 'http://media.blizzard.com/global-video-player/expressInstall.swf';
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-544112-16']);
+_gaq.push(['_setDomainName', '.battle.net']);
+_gaq.push(['_setAllowLinker', true]);
+_gaq.push(['_trackPageview']);
+//]]>
